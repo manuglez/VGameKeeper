8ff369d50d14fceb0c0fc55ad2807e4fd227e6da
Manuel Gonzalez Lara <manu.gonla@gmail.com>
2023-05-04 19:00:12 -0600

    Base Storyboard Structure

--- VGameKeeper/ViewController.swift
+++ VGameKeeper/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func barButtonSearchPressed(_ sender: Any) {
+        print("barButtonSearchPressed")
+    }
+    
 }
 
